14edbf849e00625a575d87a6352214cb9e3da3d1
AirFishWang <1013812915@qq.com>
2018-11-13 20:12:59 +0800

    web service test

--- webservice/hello_web_service.py
+++ webservice/hello_web_service.py
@@ -1,1 +1,20 @@
 # -*- coding: utf-8 -*-
+"""
+    reference: http://www.cnblogs.com/aylin/p/5702994.html
+"""
+import tornado.web
+import tornado.ioloop
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self, *args, **kwargs):
+        self.write("Hello World, web service")
+
+
+application = tornado.web.Application([
+    (r'/index', IndexHandler),
+])
+
+if __name__ == "__main__":
+    application.listen(8080)
+    tornado.ioloop.IOLoop.instance().start()
